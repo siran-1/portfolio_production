73e90405cd78ad9d5b1efdb9e8cd22fa65bacc4d
siran <siranjeevik21@gmail.com>
2023-09-15 10:51:30 +0100

    some new changes

--- source/portfolio_master.js
+++ source/portfolio_master.js
@@ -24,9 +24,9 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 /*  BLOG LEVEL GET/POST  */
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
 res.sendFile(path.resolve(__dirname, '../index.html'));
-});
+});*/
 
 app.post('/master_route', upload.array('blogPictures'), (req, res) => {
     const {action } = req.body;
